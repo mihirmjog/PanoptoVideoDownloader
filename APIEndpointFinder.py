5e553a1c13612eb3854bb7638603a118558ea819
Mihir Jog <mihirmjog@gmail.com>
2022-11-22 22:20:23 -0500

    Modified chrome capabilities to enable performance logging

--- APIEndpointFinder.py
+++ APIEndpointFinder.py
@@ -4,37 +4,42 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
 from selenium.webdriver.common.keys import Keys
-import selenium.webdriver.common.devtools.v107 as Devtools
-#import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import json
 
 class APIEndpointFinder:
     '''Provides a list of the relevant API Endpoint URLs used by the Panapoto Video Player'''
     
     def __init__(self):
+        chrome_capabilities = DesiredCapabilities.CHROME.copy()
+        chrome_capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
         chrome_options = WD.ChromeOptions()
-        chrome_options.add_argument("user-data-dir=C:/Users/mihir/AppData/Local/Google/Chrome/User Data")
+        chrome_options.add_argument("user-data-dir=C:/Users/mihir/AppData/Local/Google/Chrome/User Data") #Need to use preexisting chrome profile to autofill kerberos credentials and bypass 2FA
         chrome_options.add_argument("remote-debugging-port=9222")   
-        self.WebDriver = WD.Chrome(options = chrome_options)
-        self.wait = WebDriverWait(self.WebDriver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException]) #Defines wait conditions for Webdriver to click particular buttons
+        chrome_options.add_experimental_option('perfLoggingPrefs', {
+            'enableNetwork': True
+            }) 
+        self.WebDriver = WD.Chrome(options = chrome_options, desired_capabilities= chrome_capabilities)
         
         self.WebDriver.maximize_window()
-        Devtools.network.enable()
 
     def get_URL_list(self, panopto_video_URL): 
-        endpoint_URL_list = []
-        
+        #clear
         self.WebDriver.get(panopto_video_URL)
 
         self.__login_to_kerberos__()
         self.__mute_video__()
         self.__play_video__() 
-   
+
         if self.WebDriver.find_element(By.ID, "selectedSecondary").is_displayed(): #Checks if the panapto video has a camera expander
             self.__click_through_camera_expander__()
         else: #Video player does not have camera expander
             self.__click_through_all_cameras__()
         
+        webdriver_logs = self.WebDriver.get_log('performance')
         #TODO Check if num_cameras equals number of clicks, throw exception otherwise
+        endpoint_URL_list = self.__create_endpoint_URL_list__(webdriver_logs)
+
         return endpoint_URL_list
 
     def __login_to_kerberos__(self):
@@ -91,6 +96,8 @@ class APIEndpointFinder:
     def __is_camera_button__(self, potential_camera_button):
         return potential_camera_button.get_attribute("class") == "player-tab-header transport-button accented-tab object-video secondary-header"
     
-    def __add_endpoint_URL__(self): 
-        
-        return 0
+    def __create_endpoint_URL_list__(self, webdriver_logs): 
+        endpoint_URL_list = []
+        for log in webdriver_logs:
+            print(log)
+        return        
