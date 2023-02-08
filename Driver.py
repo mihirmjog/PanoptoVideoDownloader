467f9a0a85422d9200092a4ba6634917acc95147
Mihir Jog <mihirmjog@gmail.com>
2023-02-07 20:59:15 -0500

    Deleted __check_for_errors()

--- Driver.py
+++ Driver.py
@@ -158,26 +158,37 @@ class Driver:
             print(self.__error_log_dict.keys)
         else :
              "Success!"
-    
-    def __check_for_error(self):
-        sleep(2)
-        try:
-            error_status_elem = self.__WebDriver.get_element_when_accessible(By.XPATH, "//*[ contains (text(), ‘An Error’ ) ]")
-        except:
-            return
-        else: 
-            error_status_elem.click()
-            ActionChains.context_click(error_status_elem)
-            force_download_button = self.__WebDriver.get_element_when_accessible(By.XPATH, "/html/body/div[3]/div/div/a[4]/text()")
-            force_download_button.click()
+
 #------------------------------------------------------------------------------------------------------------------------------------------
 #Driver 
 video_downloader = Driver()
 
 #1) Set download location here:
-video_downloader.set_download_location()
+video_downloader.set_download_location(r"D:\MIT\6.5620[J] Cryptography and Cryptanalysis (F22) [6.857]\Lecture Videos")
 
 #2) Add Panopto Video URL's here and name of parent folder
+video_downloader.add("Lecture 4", "https://mit.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=addc5a32-fbca-4680-94dc-af0c00d7840f")
+video_downloader.add("Lecture 5", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ecce115e-11c5-4999-8516-af1300d72e1a")
+video_downloader.add("Lecture 6", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=12594d13-467f-4d4f-834d-af1a00d725f7")
+video_downloader.add("Lecture 7", "https://mit.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=734f395a-bd5e-459e-af7b-af1a00d7b38b")
+video_downloader.add("Lecture 8", "https://mit.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=0163f5ee-e1f6-4606-ac87-af2100d69d7e")
+video_downloader.add("Lecture 9", "https://mit.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=bd6c4da3-13e2-4132-a33c-af2100d6d9fa")
+video_downloader.add("Lecture 10", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=1a061ba5-a322-41f6-887e-af2800d69ca4")
+video_downloader.add("Lecture 11", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9fe277ca-d710-4129-a374-af2f00d698ed")
+video_downloader.add("Lecture 12", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=f34cdd78-615d-4fdb-8211-af2f00d71262")
+video_downloader.add("Lecture 13", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d752b1a9-ff56-43c1-b875-af3600d69162")
+video_downloader.add("Lecture 14", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=29fe2eb9-ce2a-43ee-99be-af3600d6d190")
+video_downloader.add("Lecture 15", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c2b9e102-a93c-49c9-80dc-af3d00d6a398")
+video_downloader.add("Lecture 16", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=92af7010-f885-4af2-b0f6-af3d00d71afd")
+video_downloader.add("Lecture 17", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=49b56d96-fa8c-4b14-b9da-af4400d69d3a&fbclid=IwAR2VXhDQAzGWZVCuozWCSgV1FKglGz0QDR9dk6VEBBdygRPRUUE7Ru2BLuU")
+#LECTURE 18 ALREADY ARCHIVED
+video_downloader.add("Lecture 19", "https://mit.zoom.us/rec/share/j5otCaGTJf5z0uhhqRHL04DIxKYaFvGALIBlYVBedFDvlca9z63k8bKSJXHvuQMU.uQY3_jD528DItTNK")
+video_downloader.add("Lecture 20", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=7c4bdf51-d534-4989-8438-af4b00d73102")
+video_downloader.add("Lecture 21", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=809ed765-b238-41cf-b0be-af5300669485")
+video_downloader.add("Lecture 22", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=359fc140-03f2-4e9a-81c9-af5900d6aeff")
+video_downloader.add("Lecture 23", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=172d2a9a-eb3d-424d-afeb-af5900d70d90&fbclid=IwAR3sRfoTgL51p5G0uHnptBvcml15li_ZLXh2S08vinNA-4EGEchRpJx2QyU")
+video_downloader.add("Lecture 24", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=93254a33-215a-44bf-9225-af6000d6c3c7")
+video_downloader.add("Lecture 25", "https://mit.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=47b1595d-d24c-47cf-b4bc-af6000d71fcf")
 
 
 #3) Start downloads
